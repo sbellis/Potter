45a630ddab2969f75cfd129b62ccdf099da0d714
Simon Bellis <simon@f-tek.co.uk>
2020-08-06 15:38:12 +0100

    Tests

--- PotterKata.Tests/BookGroupTest.cs
+++ PotterKata.Tests/BookGroupTest.cs
@@ -22,5 +22,60 @@ namespace PotterKata.Tests
 
             bookGroup.AddBookToGroup(new Book(BookTitles.DeathlyHallows)).Should().BeFalse();
         }
+
+        [Test]
+        public void CalculateGroupPrice_OneBook()
+        {
+            var bookGroup = new BookGroup(new DiscountCalculator());
+            bookGroup.AddBookToGroup(new Book(BookTitles.GobletOfFire));
+
+            bookGroup.CalculateGroupPrice().Should().Be(8m);
+        }
+
+        [Test]
+        public void CalculateGroupPrice_TwoBook()
+        {
+            var bookGroup = new BookGroup(new DiscountCalculator());
+            bookGroup.AddBookToGroup(new Book(BookTitles.GobletOfFire));
+            bookGroup.AddBookToGroup(new Book(BookTitles.ChamberOfSecrets));
+
+            bookGroup.CalculateGroupPrice().Should().Be(15.2m);
+        }
+
+        [Test]
+        public void CalculateGroupPrice_ThreeBook()
+        {
+            var bookGroup = new BookGroup(new DiscountCalculator());
+            bookGroup.AddBookToGroup(new Book(BookTitles.GobletOfFire));
+            bookGroup.AddBookToGroup(new Book(BookTitles.ChamberOfSecrets));
+            bookGroup.AddBookToGroup(new Book(BookTitles.DeathlyHallows));
+
+            bookGroup.CalculateGroupPrice().Should().Be(21.6m);
+        }
+
+        [Test]
+        public void CalculateGroupPrice_FourBook()
+        {
+            var bookGroup = new BookGroup(new DiscountCalculator());
+            bookGroup.AddBookToGroup(new Book(BookTitles.GobletOfFire));
+            bookGroup.AddBookToGroup(new Book(BookTitles.ChamberOfSecrets));
+            bookGroup.AddBookToGroup(new Book(BookTitles.DeathlyHallows));
+            bookGroup.AddBookToGroup(new Book(BookTitles.HalfBloodPrince));
+
+            bookGroup.CalculateGroupPrice().Should().Be(25.6m);
+        }
+
+        [Test]
+        public void CalculateGroupPrice_FiveBook()
+        {
+            var bookGroup = new BookGroup(new DiscountCalculator());
+            bookGroup.AddBookToGroup(new Book(BookTitles.GobletOfFire));
+            bookGroup.AddBookToGroup(new Book(BookTitles.ChamberOfSecrets));
+            bookGroup.AddBookToGroup(new Book(BookTitles.DeathlyHallows));
+            bookGroup.AddBookToGroup(new Book(BookTitles.HalfBloodPrince));
+            bookGroup.AddBookToGroup(new Book(BookTitles.OrderOfThePheonix));
+
+            bookGroup.CalculateGroupPrice().Should().Be(30m);
+        }
     }
 }
